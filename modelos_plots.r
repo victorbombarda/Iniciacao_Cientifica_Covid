f764d5608bd126f465b2f21bd7e91ea2726eb20d
Victor <70376108+victorbombarda@users.noreply.github.com>
2021-02-08 17:16:33 -0300

    Código final usado no relatório
    Acompanha alguns comentários explicando o que o código monta

--- modelos_plots.r
+++ modelos_plots.r
@@ -1,116 +1,66 @@
-###Plots comparativos
+##Código para plotagem dos gráficos comparativos
 
+#pacotes usados neste código
 library(PandemicLP)
 library(ggplot2)
-df_BR <- load_covid("Brazil")
 
-#Criei uma coluna y no dataframe para conseguir filtrá-lo
-#Ainda não pensei em uma forma de filtrar sem que seja manualmente
+#Usando uma função do PandemicLP carregamos os dados sobre a COVID
+#E criamos uma coluna provisória para ajudar na filtragem dos dados
+df_BR <- load_covid("Brazil")
 df_BR$data$y <- c(1:length(df_BR$data$date))
 
-##Até março
-#Dado sendo filtrado
-df_marco <- lapply(df_BR, subset, df_BR$data$y < 37)
+#A partir da coluna criada, filtramos os dados manualmente (Espero solucionar em breve)
+#E redefino algumas propriedades da lista para que possamos criar o modelo a partir dela
+df <- lapply(df_BR, subset, df_BR$data$y < X) #Sendo X o valor usado para filtrar
+df$name <- "Brazil"
+df$population <- 210147125
 
-#Após filtrar dessa maneira, precisei redefinir algumas variáveis
-#na lista de informação pois para cada item que não é do dataframe 
-#dos dados, aparecem n NAs (que não podem aparecer na função pandemic_model() ).
-df_marco$name <- "Brazil"
-df_marco$population <- 210147125
-model_marco <- pandemic_model(df_marco)
-predict_marco <- posterior_predict(model_marco) #Guardo em uma variável para não 
-#ter que calcular sempre que formos usar.
+#Aplicamos a lista à função do modelo
+modelo <- pandemic_model(df)
+posterior_predict(modelo)
+posterior_predict_modelo <- posterior_predict(modelo) 
 
-#Reuni as datas para além dos dados fornecidos para poder associar às previsões.
-df_post_marco <- lapply(df_BR, subset, df_BR$data$y > 36)
-datas_post_marco <- df_post_marco$data$date
+#Agregamos as datas para após o dado coletado para o modelo
+df_post <- lapply(df_BR, subset, df_BR$data$y > (X-1) )#Datas subsequentes
+datas_post <- df_post$data$date
 
-#Peguei as previsões do modelo fornecido
-#VER DADOS DA MATRIZ E ENTENDER
-previsoes_post_marco <- predict_marco$predictive_Long[2,]
-
-#Criado dataframe com previsões
-df_previsoes_post_marco <- data.frame(datas = datas_post_marco, previsoes = previsoes_post_marco[1:length(datas_post_marco)])
+#Extraimos os dados gerados pela previsão
+previsoes_post <- posterior_predict_modelo$predictive_Long[2,]
+df_previsoes_post <- data.frame(datas = datas_post, previsoes = previsoes_post[1:length(datas_post)])
 
 #Grafico
-ggplot() +
+ggplot() + 
   
-  geom_point(df_BR$data, mapping= aes(x = df_BR$data$date, y =df_BR$data$new_cases , group = 1, color = "Dados reais observados")) +
-  geom_line(df_BR$data, mapping= aes(x = df_BR$data$date, y =df_BR$data$new_cases , group = 1, color = "Dados reais observados")) +
+  #Plotados os dados reais observados
+  geom_point(df_BR$data, mapping= aes(x = df_BR$data$date,
+                                      y =df_BR$data$new_cases ,
+                                      group = 1, color = "Dados reais observados")) +
   
-  geom_point(predict_marco$data, mapping = aes(x = date, y = new_cases,group = 2, color = "Dados fornecidos ao modelo")) +
-  geom_line(predict_marco$data, mapping = aes(x = date, y = new_cases,group = 2, color = "Dados fornecidos ao modelo")) +
+  geom_line(df_BR$data, mapping= aes(x = df_BR$data$date,
+                                     y =df_BR$data$new_cases ,
+                                     group = 1, color = "Dados reais observados")) +
   
-  geom_point(df_previsoes_post_marco, mapping =  aes(x = df_previsoes_post_marco$datas, y = df_previsoes_post_marco$previsoes,group = 3, color = "Previsões do Modelo")) +
-  geom_line(df_previsoes_post_marco, mapping = aes(x = df_previsoes_post_marco$datas, y = df_previsoes_post_marco$previsoes,group = 3, color = "Previsões do Modelo"))
-
-
-###Até maio
-df_maio <- lapply(df_BR, subset, df_BR$data$y < 98)
-df_maio$name <- "Brazil"
-df_maio$population <- 210147125
-model_maio <- pandemic_model(df_maio)
-a1 <- posterior_predict(model_maio)
-
-df_post_maio <- lapply(df_BR, subset, df_BR$data$y > 97)
-datas_post_maio <- df_post_maio$data$date
-
-previsoes_post_maio <- a1$predictive_Long[2,]
-df_previsoes_post_maio <- data.frame(datas = datas_post_maio, previsoes = previsoes_post_maio[1:length(datas_post_maio)])
-
-ggplot() +
+  #Plotados a informação dado ao modelo
+  geom_point(posterior_predict_modelo$data, mapping = aes(x = date,
+                                                          y = new_cases,
+                                                          group = 2, color = "Dados fornecidos ao modelo")) +
   
-  geom_point(df_BR$data, mapping= aes(x = df_BR$data$date, y =df_BR$data$new_cases , group = 1, color = "Dados reais observados")) +
-  geom_line(df_BR$data, mapping= aes(x = df_BR$data$date, y =df_BR$data$new_cases , group = 1, color = "Dados reais observados")) +
-  geom_point(a1$data, mapping = aes(x = date, y = new_cases,group = 2, color = "Dados fornecidos ao modelo")) +
-  geom_line(a1$data, mapping = aes(x = date, y = new_cases,group = 2, color = "Dados fornecidos ao modelo")) +
-  geom_point(df_previsoes_post_maio, mapping =  aes(x = df_previsoes_post_maio$datas, y = df_previsoes_post_maio$previsoes,group = 3, color = "Previsões do Modelo")) +
-  geom_line(df_previsoes_post_maio, mapping = aes(x = df_previsoes_post_maio$datas, y = df_previsoes_post_maio$previsoes,group = 3, color = "Previsões do Modelo"))
-
-
-
-
-###Até agosto
-df_agosto <- lapply(df_BR, subset, df_BR$data$y < 159)
-df_agosto$name <- "Brazil"
-df_agosto$population <- 210147125
-model_agosto <- pandemic_model(df_agosto)
-a2 <- posterior_predict(model_agosto)
-
-df_post_agosto <- lapply(df_BR, subset, df_BR$data$y > 158)
-datas_post_agosto <- df_post_agosto$data$date
-
-previsoes_post_agosto <- a2$predictive_Long[2,]
-df_previsoes_post_agosto <- data.frame(datas = datas_post_agosto, previsoes = previsoes_post_agosto[1:length(datas_post_agosto)])
-
-ggplot() +
+  geom_line(posterior_predict_modelo$data, mapping = aes(x = date,
+                                                         y = new_cases,
+                                                         group = 2, color = "Dados fornecidos ao modelo")) +
   
-  geom_point(df_BR$data, mapping= aes(x = df_BR$data$date, y =df_BR$data$new_cases , group = 1, color = "Dados reais observados")) +
-  geom_line(df_BR$data, mapping= aes(x = df_BR$data$date, y =df_BR$data$new_cases , group = 1, color = "Dados reais observados")) +
-  geom_point(a2$data, mapping = aes(x = date, y = new_cases,group = 2, color = "Dados fornecidos ao modelo")) +
-  geom_line(a2$data, mapping = aes(x = date, y = new_cases,group = 2, color = "Dados fornecidos ao modelo")) +
-  geom_point(df_previsoes_post_agosto, mapping =  aes(x = df_previsoes_post_agosto$datas, y = df_previsoes_post_agosto$previsoes,group = 3, color = "Previsões do Modelo")) +
-  geom_line(df_previsoes_post_agosto, mapping = aes(x = df_previsoes_post_agosto$datas, y = df_previsoes_post_agosto$previsoes,group = 3, color = "Previsões do Modelo"))
-
-
-###Até outubro
-df_outubro <- lapply(df_BR, subset, df_BR$data$y < 220)
-df_outubro$name <- "Brazil"
-df_outubro$population <- 210147125
-model_outubro <- pandemic_model(df_outubro)
-a3 <- posterior_predict(model_outubro)
-
-df_post_outubro <- lapply(df_BR, subset, df_BR$data$y > 219)
-datas_post_outubro <- df_post_outubro$data$date
-
-previsoes_post_outubro <- a3$predictive_Long[2,]
-df_previsoes_post_outubro <- data.frame(datas = datas_post_outubro, previsoes = previsoes_post_outubro[1:length(datas_post_outubro)])
-
-ggplot() +
+  #Plotadas as previsões
+  geom_point(df_previsoes_post, mapping =  aes(x = df_previsoes_post$datas,
+                                               y = df_previsoes_post$previsoes,
+                                               group = 3, color = "Previsões")) +
   
-  geom_point(df_BR$data, mapping= aes(x = df_BR$data$date, y =df_BR$data$new_cases , group = 1, color = "Dados reais observados")) +
-  geom_line(df_BR$data, mapping= aes(x = df_BR$data$date, y =df_BR$data$new_cases , group = 1, color = "Dados reais observados")) +
-  geom_point(a3$data, mapping = aes(x = date, y = new_cases,group = 2, color = "Dados fornecidos ao modelo")) +
-  geom_line(a3$data, mapping = aes(x = date, y = new_cases,group = 2, color = "Dados fornecidos ao modelo")) +
-  geom_point(df_previsoes_post_outubro, mapping =  aes(x = df_previsoes_post_outubro$datas, y = df_previsoes_post_outubro$previsoes,group = 3, color = "Previsões do Modelo")) +
-  geom_line(df_previsoes_post_outubro, mapping = aes(x = df_previsoes_post_outubro$datas, y = df_previsoes_post_outubro$previsoes,group = 3, color = "Previsões do Modelo"))
+  geom_line(df_previsoes_post, mapping = aes(x = df_previsoes_post$datas,
+                                             y = df_previsoes_post$previsoes
+                                             ,group = 3, color = "Previsões"))+
+  
+  #Código que mexe com a estética do gráfico  
+  labs(x="Data",y="Incidência de novos casos") +
+  theme_bw(base_size = 16)+
+  scale_x_date() + scale_y_continuous(expand = c(0,0), limits = c(0,73000)) +
+  ggtitle("Comparação com modelo - Até Março") + 
+  guides(colour=guide_legend(title="Legenda"))
